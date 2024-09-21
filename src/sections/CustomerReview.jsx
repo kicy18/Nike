407dc48443cc84a8a70d92708fa341c61ebc6c69
kicy <80158936+kicy18@users.noreply.github.com>
2024-09-21 22:04:33 +0530

    Completed

--- src/sections/CustomerReview.jsx
+++ src/sections/CustomerReview.jsx
@@ -1,8 +1,27 @@
+import ReviewCard from "../components/ReviewCard"
+import { reviews } from "../constants"
 
 
 const CustomerReview = () => {
   return (
-    <div>CustomerReview</div>
+    <section className="max-container">
+      <h3 className="font-palanquin text-center text-4xl font-bold">
+        What Our <span className="text-coral-red">Customers</span> Say?</h3> 
+      <p className="info-text m-auto mt-4 max-w-lg text-center">With an extensive collection from top brands and state-of-the-art designs, we ensure fashion.</p>
+    <div className="mt-24 flex flex-1 justify-evenly items-start max-lg:flex-col gap-14">
+      {reviews.map((review)=>(
+        <ReviewCard key = {review.customerName}
+        imgURL = {review.imgURL}
+        customerName = {review.customerName}
+        rating = {review.rating}
+        feedback = {review.feedback}
+          />
+
+
+      ))}
+
+    </div>
+    </section>
   )
 }
 
